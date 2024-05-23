9efb04b3d2149046cd8e241cad0a2bfa1a50d7bf
GyovanneMS <gyovannecontato@gmail.com>
2024-05-23 15:29:05 -0300

    Starting

---
+++ src/pages/Map.jsx
@@ -0,0 +1,6 @@
+import { View, StyleSheet, Text } from "react-native";
+import React, {useEffect, useState} from "react";
+
+export default function Map(){
+    return <Text>Map</Text>
+}
---
+++ src/routes/index.jsx
@@ -0,0 +1,42 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../pages/Home";
+import Map from "../pages/Map";
+import {Entypo, FontAwesome6} from "@expo/vector-icons";
+
+
+const Tab = createBottomTabNavigator();
+
+export function Routes(){
+    return <Tab.Navigator
+                screenOptions={{
+                    headerShown: false,
+                    tabBarStyle: {
+                        backgroundColor: "purple",
+                    },
+                    tabBarActiveTintColor: "yellow",
+                    tabBarInactiveTintColor: "gray",
+                }}>
+        <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+                tabBarIcon: ({ focused, size, color}) => {
+                    if(focused)
+                        return <Entypo name="hand" size={size} color={color} />
+                    else
+                        return <Entypo name="grid" size={size} color={color} />
+                }
+            }}/>
+        <Tab.Screen
+            name="Map"
+            component={Map}
+            options={{
+                tabBarIcon: ({ focused, size, color}) => {
+                    if(focused)
+                        return <FontAwesome6 name="explosion" size={size} color={color} />
+                    else
+                        return <Entypo name="globe" size={size} color={color} />
+                }
+            }}/>
+    </Tab.Navigator>
+}
