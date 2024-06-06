dbfd34ac5d7473ef6586254b50709bb246abd06c
GyovanneMS <gyovannecontato@gmail.com>
2024-06-06 15:22:00 -0300

    Working with Maps

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,6 +1,61 @@
-import { View, StyleSheet, Text } from "react-native";
-import React, {useEffect, useState} from "react";
+import { View, Text, Image, Pressable } from "react-native";
+import React, {useEffect, useState, useRef} from "react";
+import MapView, {Marker} from "react-native-maps";
+import * as Location from "expo-location";
+import estiloMap from"../style/Map.jsx";
+import ponteiro from "../assets/adaptive-icon.png";
+import {Ionicons, Entypo, FontAwesome6} from "@expo/vector-icons";
+//Entypo - home
 
 export default function Map(){
-    return <Text>Map</Text>
+
+    const [ localizacao, definirLocalizacao ] = useState({})
+    const icon = useRef(false)
+    
+    useEffect(function() {
+        async function ObterLocalizacao() {
+          await Location.requestForegroundPermissionsAsync()
+          definirLocalizacao(await Location.getCurrentPositionAsync({}))
+        }
+        ObterLocalizacao();
+      }, [])
+    return <View style={ estiloMap.tela }>
+        { 
+            Object.keys(localizacao).length > 0 &&
+            <>
+                <View style={ estiloMap.indicador }>
+                    <Text style={ estiloMap.indicardorTexto }> Distância da empresa até minha moradia </Text>
+                    <Text style={ estiloMap.indicardorTexto }> Latitude: { localizacao.coords.latitude } </Text>
+                    <Text style={ estiloMap.indicardorTexto }> Longitude: { localizacao.coords.longitude } </Text>
+                    <Text style={ estiloMap.indicardorTexto }> Altitude: { localizacao.coords.altitude } </Text>
+                    {/* <Image 
+                        source="require(../../assets/ponter.webp)"
+                        style={estiloMap.Imagem}/> */}
+                </View>
+                <MapView
+                initialRegion={{
+                    latitude: localizacao.coords.latitude,
+                    longitude: localizacao.coords.longitude,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                }}
+                style={ estiloMap.mapa } >
+                    <Marker
+                        key={0}
+                        coordinate={{
+                            latitude: localizacao.coords.latitude,
+                            longitude: localizacao.coords.longitude
+                            }}
+                            title="Aqui"
+                            description="aqui">
+                                <Pressable
+                                    onPressIn={icon = true}>
+                                    <Ionicons name="business" size={32} color={"black"} />
+                                </Pressable>
+                            </Marker>
+                </MapView>
+            </>
+        }
+      </View>
 }
+
---
+++ src/style/Map.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native"
+
+const estiloMap = StyleSheet.create({
+    tela: { flex: 1 },
+    indicador: { backgroundColor: "#144272", padding: 32 },
+    indicardorTexto: { color: "white", fontSize: 20 },
+    mapa: { height: "100%", width: "100%" },
+    Imagem: {height: 30, width: 30, backgroundColor: "#FFFF00"}
+})
+
+export default estiloMap;
